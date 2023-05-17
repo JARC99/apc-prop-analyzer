ee3fd33255002b2dc22fd9ca273d01078bc979e5
José A. Rosas <jarosascordova@gmail.com>
2023-05-17 14:48:14 -0600

    Update plot style

--- convperf_plots.py
+++ convperf_plots.py
@@ -1,37 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 25 17:35:19 2022
-
-@author: jaros
-"""
-
-"""Plot the propeller selection diagrams."""
-# %% Preamble
 import seaborn as sns
 import matplotlib.pylab as plt
 import numpy as np
 import os
 
-from scipy import interpolate
-from pint import UnitRegistry
-from tabulate import tabulate
-
 # Plotting parameters
 DPI = 300
-SUBSTACK_FLAG = True
-FACECOLOR = "#f2f2e3"
-COLORS = ["darkblue", "darkorange", "darkgreen", "firebrick",
-          "purple", "mediumvioletred", "goldenrod", "darkcyan"]
+PALETTE = ["darkblue", "firebrick", "forestgreen", "darkorange",
+           "darkcyan", "rebeccapurple", "mediumvioletred"]
 MARKERS = ['o', '^', 's', 'P', 'd']
 LEGEND_FONTSIZE = "small"
-GRAPHICS_DIR = "graphics"
+GRAPHICS_DIR = "output"
 
-if SUBSTACK_FLAG:
-    sns.set_theme(style="whitegrid", font="monospace",
-                  context="paper", palette=COLORS)
-else:
-    sns.set_theme(style="whitegrid", font="Palatino Linotype",
-                  context="paper", palette=COLORS)
+sns.set_theme(style="whitegrid", font="Palatino Linotype",
+              context="paper", palette=PALETTE)
 
 # Data directory
 AVEPERF_DATA_DIR = "reformatted_data/ave_performance"
@@ -40,12 +21,12 @@ AVEPERF_DATA_DIR = "reformatted_data/ave_performance"
 # %% User input
 
 # Declare the propeller family
-prop_list = ["17x6E", "17x8E", "17x12E"]
+prop_list = ["D17P6B2TE"]
 
 
 # %% Computations and plotting
 
-fig, axes = plt.subplots(3, sharex=True, dpi=DPI, facecolor=FACECOLOR)
+fig, axes = plt.subplots(3, sharex=True, dpi=DPI)
 ax1 = axes[0]
 ax2 = axes[1]
 ax3 = axes[2]
@@ -68,25 +49,13 @@ for k, prop in enumerate(prop_list):
     ax2.plot(J_array, CP_array)
     ax3.plot(J_array, eta_p_array)
 
-    if SUBSTACK_FLAG:
-        for ax in axes:
-            for spine in ax.spines.values():
-                spine.set(color="gray")
-            ax.set_facecolor(FACECOLOR)
-            ax.grid(visible=True, color="gray")
-        ax1.legend(title="Propeller", facecolor=FACECOLOR, fancybox=False,
-                   edgecolor="gray", loc="upper right",
-                   fontsize=LEGEND_FONTSIZE, title_fontsize=LEGEND_FONTSIZE)
-
-    else:
-        ax1.legend(loc="upper right",
+    ax1.legend(loc="upper right",
                    fontsize=LEGEND_FONTSIZE, title_fontsize=LEGEND_FONTSIZE)
 
 
 for ax in axes:
     ax.set_xlim(left=0)
     ax.set_ylim(bottom=0)
-
 
 
 # %% Output
